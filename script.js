f50d2303ee42e03b86301dda2c3c429cc1d26901
Elise Offerman <eliseofferman@Elises-MacBook-Pro.local>
2018-03-21 13:04:42 +0100

    Js file added

---
+++ script.js
@@ -0,0 +1,9 @@
+// First select the cards
+// Attach click event
+// Toggle the class to is-switched
+// Toggle card children to is-active
+function toggle() {
+this.classList.toggle("is-switched")
+}
+
+document.getElementById("img1").onclick = toggle
